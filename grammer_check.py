48d7dccd7ec1498f0e7c65c92b25cc871a0fc9b5
KzKe <kangk1@student.unimelb.edu.au>
2017-08-15 10:57:06 +1000

    Convert the code in to a function

--- grammer_check.py
+++ grammer_check.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 
 '''
-Install Python package language-check first.
+Install Python package language-check first.https://pypi.python.org/pypi/language-check
 This code only run under python3.
-https://pypi.python.org/pypi/language-check
-This code is too slow.
 '''
 import language_check
-tool = language_check.LanguageTool('en-US')
-text = u'A sentence with a error the the in the Hitchhiker’s Guide to the the Galaxy'
-matches = tool.check(text)
-# print(matches[1])
 
-# output all the grammer errors
-# for item in matches:
-#     print (item)
+# input a sentence list
+# return the number of grammar errors
+def text_grammar_check(text):
+    tool = language_check.LanguageTool('en-US')
+    total_mistakes = 0
+    for s in text:
+        total_mistakes += sent_grammar_check(tool, s)
+    return total_mistakes
 
-# print out the number of grammer errors.
-print(len(matches))
+def sent_grammar_check(tool, s):
+    matches = tool.check(s)
+    return len(matches)
+
+# Examples
+# text = u'A sentence with a error the the in the Hitchhiker’s Guide to the the Galaxy'
+# print (text_grammar_check([text]))
