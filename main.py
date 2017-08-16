415262cffee010838300729f6c017cc79c365429
KzKe <kangk1@student.unimelb.edu.au>
2017-08-16 15:57:03 +1000

    update the code.

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from essay_features import inf_summary
+
 
 # text = "Hello world! this is a test. This is another test."
 # print (inf_summary(text))
 
 # load the data, training set and test set
-
+training_set = pd.read_csv
 
 # create and train a model
 
